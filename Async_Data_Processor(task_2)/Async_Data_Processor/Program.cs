49aea1f12d1a9c83cab56db96498764c994c4f89
KuchN <mrkola45@gmail.com>
2025-10-02 16:22:04 +0300

    first week tasks

---
+++ Async_Data_Processor(task_2)/Async_Data_Processor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace Async_Data_Processor
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+            Console.WriteLine("Синхронная обработка:");
+            timer.Start();
+            TestingData();
+            timer.Stop();
+            Console.WriteLine($"Обработка заняла : {timer.Elapsed.TotalSeconds} секунд");
+
+            Console.WriteLine("\nАсинхронная обработка:");
+            timer.Restart();
+            timer.Start();
+            await TestingDataAsync(); 
+            timer.Stop();
+            
+            Console.WriteLine($"Обработка заняла : {timer.Elapsed.TotalSeconds} секунд");
+        }
+
+        public static void ProcessData(string Data_Name)
+        {
+            Console.WriteLine($" Начало обработки {Data_Name}");
+            Thread.Sleep(3000);
+            Console.WriteLine($" {Data_Name} успешно обработан!");
+        }
+        async public static Task ProcessDataAsync(string Data_Name)
+        {
+            Console.WriteLine($" Начало обработки {Data_Name}");
+            await Task.Delay(3000); //передаем управление далее синхронному коду,пока не выполнится таск
+            Console.WriteLine($" {Data_Name} успешно обработан!");
+        }
+        public static void TestingData()
+        {
+
+            ProcessData("dataset1.json");
+            ProcessData("dataset2.json");
+            ProcessData("dataset3.json");
+
+        }
+        public async static Task TestingDataAsync()
+        {
+            
+            var task1 = ProcessDataAsync("dataset1.json");
+            var task2 = ProcessDataAsync("dataset2.json");
+            var task3 = ProcessDataAsync("dataset3.json");
+
+            await Task.WhenAll(task1, task2, task3);//таск выполнится при завершении всех вложенных
+            
+        }
+    }
+}
---
+++ Console_Calculator(task_1)/Calculator/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double var1, var2;
+            string action;
+
+
+            do
+            {
+                var1 = NumberInput();
+                action = ActionInput();
+                var2 = NumberInput();
+
+                Calculate(var1, var2, action);
+                action = CalcActionInput();
+            } while (action == "1");
+           
+        }
+        public static double NumberInput()
+        {
+            
+            double operation;
+            while (true)
+            {
+                Console.WriteLine("Введите число (например: 1,1):");
+                if (  double.TryParse(Console.ReadLine(),out operation))
+                    return operation;
+                Console.WriteLine("Неверный формат числа, попробуйте снова.");
+
+            }
+        }
+        public static string ActionInput()
+        {
+            string  action = "";
+            bool accepted_format = false;
+            while (!accepted_format)
+            {
+                Console.WriteLine("Выберите операцию +,-,*,/");
+                action = Console.ReadLine();
+                if (action != "+" && action != "-" && action != "*" && action != "/")
+                {
+                    Console.WriteLine("Данные введены в неверном формате! Повторите ввод.");
+                }
+                else
+                {
+                    accepted_format = true;
+                }
+                
+            }
+            return action;
+        }
+        public static string CalcActionInput()
+        {
+            
+            string action = "";
+            bool accepted_format = false;
+            while (!accepted_format)
+            {
+                Console.WriteLine("Выберите операцию 1 - новая операция, 0 - закончить работу");
+                action = Console.ReadLine();
+                if (action != "1" && action != "0")
+                {
+                    Console.WriteLine("Данные введены в неверном формате! Повторите ввод.");
+                }
+                else
+                {
+                    accepted_format = true;
+                }
+                
+            }
+            return action;
+        }
+        public static void Calculate(double? x,double? y,string action_type)
+        {
+            Console.WriteLine("Результат:");
+            switch (action_type)
+            {
+
+                case "+": Console.WriteLine(x + y); break;
+                case "-": Console.WriteLine(x - y); break;
+                case "*": Console.WriteLine(x * y); break;
+                case "/":
+                    {
+                        if (y == 0) { Console.WriteLine("Невозможно делить на ноль!");break; }
+                        Console.WriteLine(x / y); break;
+                    }
+            }
+        }
+
+    }
+
+   
+}
